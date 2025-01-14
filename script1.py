aa0e159526dc9eac0c76d8a20ef7d739fb732f5c
gkaur201 <156548687+gkaur201@users.noreply.github.com>
2025-01-14 13:29:10 -0500

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added print() statements to scropt2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #print lines from zen python.
 print("If the implementation is hard to explain, it's a bad idea.")
-print("If the implementation is easy to explain, it may be a good idea.")
--- script2.py
+++ script2.py
@@ -2,3 +2,4 @@
 print("Although that way may not be obvious at first unless you're Dutch").
 print("Now is better than never.")
 print("Although never is often better than *right* now.")
+print("Beautifull is better than ugly")
