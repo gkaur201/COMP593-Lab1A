1df28abea9899d9494d306410c5eaca623ff3a66
gkaur201 <156548687+gkaur201@users.noreply.github.com>
2025-01-14 13:09:21 -0500

    Putt comment back in script2.py
    Comment was unintentionally deleted by revert.

--- script2.py
+++ script2.py
@@ -1,4 +1,4 @@
-#print another lines from zen python
+#This is good programing advice.
 print("Although that way may not be obvious at first unless you're Dutch").
 print("Now is better than never.")
 print("Although never is often better than *right* now.")
