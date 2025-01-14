f471f3db35261baf3c05f2e676ecf6b79019cc6c
gkaur201 <156548687+gkaur201@users.noreply.github.com>
2025-01-14 12:52:33 -0500

    Created script files
    Added code that prints from Zen of Python

---
+++ script1.py
@@ -0,0 +1,3 @@
+#print lines from zen python
+print("If the implementation is hard to explain, it's a bad idea.")
+print("If the implementation is easy to explain, it may be a good idea.")
---
+++ script2.py
@@ -0,0 +1,4 @@
+#print another lines from zen python
+print("Although that way may not be obvious at first unless you're Dutch").
+print("Now is better than never.")
+print("Although never is often better than *right* now.")
