99063709eda24b4d4f567c76589d591f51bbed1e
gkaur201 <156548687+gkaur201@users.noreply.github.com>
2025-01-14 13:02:48 -0500

    Modified both script files
    -Deleted print from script1.py
    -Added comment to script2.py

--- script2.py
+++ script2.py
@@ -1,4 +1,4 @@
-#print another lines from zen python
+#print another lines from zen python.
 print("Although that way may not be obvious at first unless you're Dutch").
 print("Now is better than never.")
 print("Although never is often better than *right* now.")
